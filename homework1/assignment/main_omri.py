72972f953bb08e9aa7542077cd9a42912c322ff2
Omri Gruman <OmriGr1515@gmail.com>
2022-11-15 00:03:00 +0200

    Implemented probability counting for 4-grams

--- homework1/assignment/main_omri.py
+++ homework1/assignment/main_omri.py
@@ -1,52 +1,60 @@
 import json
 import re
-from collections import defaultdict
-from time import time
-start = time()
+import numpy as np
 
-N = 2
+from time import time
+
+start = time()
+print()
+
+N = 4
 BLANK = '__________'
 
 
-def read_files(cloze, candidates, corpus):
-    print("Read cloze..")
-    with open(cloze, 'r', encoding='utf-8') as f:
-        cloze_text = f.read()
-    print("Read candidates..")
-    with open(candidates, 'r', encoding='utf-8') as f:
-        candidate_words = f.read().split()
-    print("Read corpus..")
-    with open(corpus, 'r', encoding='utf-8') as f:
-        corpus_text = f.read()
-    return cloze_text, candidate_words, corpus_text
-
-
 def find_cloze_preceding_words(cloze_text):
-    return [match.group() for match in re.finditer(fr'(\S+\s){{{N-1}}}(?={BLANK})', cloze_text)]
+    return [match.group().split() for match in re.finditer(fr'(\S+\s){{{N-1}}}(?={BLANK})', cloze_text)]
 
 
-def count_ngram_ending_freqs(text, ngram_beginnings, candidate_words):
-    ending_freqs = {begin: defaultdict(int) for begin in ngram_beginnings}
+def count_ngram_ending_freqs(corpus, ngram_beginnings, candidate_words):
+    probabilities = np.zeros((len(ngram_beginnings), N-1, len(candidate_words)))
 
-    for match in re.finditer(fr'(\S+\s){{{N-1}}}(?=\S+)', text):
-        match = match.group()
-        beginning = match[:match.rfind(' ')+1]
-        ending = match[match.rfind(' ')+1:]
-        if beginning in ngram_beginnings and ending in candidate_words:
-            ending_freqs[beginning][ending] += 1
+    with open(corpus, 'r', encoding='utf-8') as f:
+        window = [""] * N
+        for i, line in enumerate(f):
+            for word in line.split():
+                window = window[1:]
+                if word in candidate_words:
+                    for n in range(N-1):
+                        for b, begin in enumerate(ngram_beginnings):
+                            if window[N-1-n-1:] == begin[N-1-n-1:]:
+                                probabilities[b][n][candidate_words.index(word)] += 1
+                window.append(word)
 
-    return ending_freqs
+            if i % 100000 == 0:
+                print(f'[{(time() - start):.2f}] {i}')
+
+    return probabilities
 
 
 def solve_cloze(cloze, candidates, lexicon, corpus):
-    # todo: implement this function
-    print(f'starting to solve the cloze {cloze} with {candidates} using {lexicon} and {corpus}')
+    print(f'[{(time() - start):.2f}] starting to solve the cloze {cloze} with {candidates} using {lexicon} and {corpus}')
 
-    cloze_text, candidate_words, corpus_text = read_files(cloze, candidates, corpus)
+    with open(cloze, 'r', encoding='utf-8') as f:
+        cloze_text = f.read()
+    with open(candidates, 'r', encoding='utf-8') as f:
+        candidate_words = f.read().split()
+
     ngram_beginnings = find_cloze_preceding_words(cloze_text)
     print(ngram_beginnings)
-    ngram_frequencies = count_ngram_ending_freqs(corpus_text, ngram_beginnings, candidate_words)
-    print(ngram_frequencies)
+
+    probabilities = count_ngram_ending_freqs(corpus, ngram_beginnings, candidate_words)
+
+    for b, begin in enumerate(ngram_beginnings):
+        print(f'-------- {begin}')
+        for n in range(N-1):
+            print(f'---- {begin[N-1-n-1:]}')
+            for c, candidate in enumerate(candidate_words):
+                print(f'{candidate} = {probabilities[b][n][c]}')
 
     return list()  # return your solution
 
